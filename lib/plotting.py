5e3561f28abe6ffe40611c7881aaac741cd06eab
MikkelHansen95 <mikkelhansen95@live.dk>
2018-11-04 15:04:33 +0100

    mangler 4 og 6 ses

--- lib/plotting.py
+++ lib/plotting.py
@@ -7,6 +7,11 @@ from matplotlib.ticker import FuncFormatter
 
 
 def bar_plot_public_vs_private(y1,y2):
+    plot_file = f'plot0.png'
+    plot_dir = 'plots'
+    # If the plot directory is not already present it will be created.
+    if not os.path.isdir(plot_dir):
+        os.makedirs(plot_dir)
 
     my_sum = sum(y1) + sum(y2)
     new_y1 = [(x/my_sum)*100 for x in y1]
@@ -26,10 +31,15 @@ def bar_plot_public_vs_private(y1,y2):
     ax.set_xticklabels( ('Vesterbro', 'Indre By', 'Brønshøj-Husum','Vanløse','Valby','Amager Øst','Amager Vest','Østerbro','Nørrebro','Bispebjerg'))
     plt.xticks(rotation=60)
     ax.legend(['Private','Public'])
+    plt.savefig(os.path.join(plot_dir, plot_file))
     plt.show()
+    plt.close()
 def plot_parking_vs_income(df_private,df_ecar):
-    #x = 10
-    #index = np.arange(x)
+    plot_file = f'plot1.png'
+    plot_dir = 'plots'
+    # If the plot directory is not already present it will be created.
+    if not os.path.isdir(plot_dir):
+        os.makedirs(plot_dir)
  
     ax = plt.gca()
     df_p = df_private.sort_values('earnings',ascending=True)
@@ -43,19 +53,6 @@ def plot_parking_vs_income(df_private,df_ecar):
     plt.xticks(rotation=45)
     ax.set_xticklabels(df_p['city_name'], minor=False)
     ax.set_xticklabels(df_p['earnings'], minor=True)
-
+    plt.savefig(os.path.join(plot_dir, plot_file))
     plt.show()
-    '''
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    ax.plot(index, private_list, color='royalblue')
-    ax.plot(index, ecars, color='seagreen')
-    plt.xticks(rotation=60)
-    ax.set_xticks(index)
-    ax.set_xlabel('average earnings')
-    ax.set_ylabel('Number of parking spots')
-    ax.set_xticklabels( ('Vesterbro', 'Indre By', 'Brønshøj-Husum','Vanløse','Valby','Amager Øst','Amager Vest','Østerbro','Nørrebro','Bispebjerg'))
-    ax.legend(['Privat', 'El-bil'])
-    plt.show()
-    '''
+    plt.close()
